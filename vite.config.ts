38c8290798fd86f31d7aeaf42fbdf3eab85ae23c
leopalatto20 <A01665462@tec.mx>
2024-11-25 12:16:56 -0600

    deploy ahora si

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://leopalatto20.github.io/wireframe',
+  base: '/wireframeIot/',
   plugins: [react()],
 })
